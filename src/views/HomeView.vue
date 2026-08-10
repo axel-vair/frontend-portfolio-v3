331cc632116be43d4941e4807cb319fe1ca168fa
Axel <axel@Macadamia.local>
2026-08-10 16:00:13 +0200

    Ajout de la section stack

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import AppHeader from '@/components/layout/AppHeader.vue'
 import HeroSection from '@/components/sections/HeroSection.vue'
+import SkillsSection from '@/components/sections/SkillsSection.vue'
 </script>
 
 <template>
     <AppHeader />
     <HeroSection />
+    <SkillsSection />
 </template>
