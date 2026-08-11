f03777021aa5dda959ef4e0188be5d539b21a76a
Axel <axel@Macadamia.local>
2026-08-11 14:42:00 +0200

    Ajout de la partie contact

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,12 @@
 import AppHeader from '@/components/layout/AppHeader.vue'
 import HeroSection from '@/components/sections/HeroSection.vue'
 import SkillsSection from '@/components/sections/SkillsSection.vue'
+import ContactSection from '@/components/sections/ContactSection.vue'
 </script>
 
 <template>
     <AppHeader />
     <HeroSection />
     <SkillsSection />
+    <ContactSection />
 </template>
